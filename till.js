483e30e42b65188d23d23049cfae455f13fc01d4
Ted Yee <tedyee114@gmail.com>
2023-11-30 17:29:09 -0500

    added header import for scalable matching headers

--- till.js
+++ till.js
@@ -18,10 +18,39 @@ function rotateElement(event, element) {
   // get offset from middle as a percentage
   // and tone it down a little
   const offsetX = ((x- middleX) / middleX) * 45;
-  const offsetY = ((y-500) / middleY) * 45;
+  const offsetY = ((y-middleY) / middleY) * 45;
   // console.log(offsetX, offsetY);
 
   // set rotation
   element.style.setProperty("--rotateX", offsetX + "deg");
   element.style.setProperty("--rotateY", -1 * offsetY + "deg");
+}
+
+//This script defined the function "includeHTML to import other html files
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+  elmnt = z[i];
+  /*search for elements with a certain atrribute:*/
+  file = elmnt.getAttribute("w3-include-html");
+  if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+      }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+  }
+  }
 }
