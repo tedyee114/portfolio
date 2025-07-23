0f64b63d977ed19183342f3ae2c4296346558f73
Ted Yee <tedyee114@gmail.com>
2025-07-23 13:41:34 -0400

    added cursor and made companie sbar gray until hover

--- till.js
+++ till.js
@@ -189,3 +189,24 @@ document.addEventListener('DOMContentLoaded', function() {
     // Call includeHTML if needed
     includeHTML();
 });
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+// document.addEventListener('click', () => {
+//     cursor.classList.add("expand");
+
+//     setTimeout(() => {
+//         cursor.classList.remove("expand");
+//     }, 500)
+// })
+
+$(document).mousemove(function(e) {
+  $("#cursorinner").css({
+    left: e.pageX,
+    top: e.pageY
+  });
+});
